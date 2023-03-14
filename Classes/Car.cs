3bdb140fd1ebb897b9482bf43a274069a25798ab
adamacneill <adamichelee@gmail.com>
2023-03-13 18:43:45 -0700

    initial commit

---
+++ Classes/Car.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classes
+{
+    public class Car
+    {
+        public Car()
+        {
+
+        }
+        public Car(string makeInput, string modelInput, int  YearInput)
+        {
+            Make = Make;
+            Model = Model;
+            Year = Year;
+        }
+
+
+        public string Make { get; set; }    
+        
+
+        public string Model { get; set; }
+
+
+        public int Year { get; set; }
+
+    }
+}
--- Classes/Program.cs
+++ Classes/Program.cs
@@ -1,10 +1,37 @@
-﻿namespace Classes
+﻿using Microsoft.VisualBasic;
+
+namespace Classes
 {
     public class Program
     {
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var myCar = new Car();
+
+            myCar.Make = "Ford";
+
+            myCar.Model = "Focus";
+
+            myCar.Year = 2011;
+
+            var subaru = new Car();
+            {
+                string Make = "Subaru";
+                string Model = "Crosstrek";
+                int Year = 2023;
+            };
+
+            var mazda = new Car("Mazda", "Miata", 2001);
+
+            var carList = new List<Car>() { myCar, subaru, mazda };
+
+            foreach(var vehicle in carList)
+
+            {
+                Console.WriteLine($"{vehicle.Make}, {vehicle.Model}, {vehicle.Year}");
+            }
+
         }
     }
 }
